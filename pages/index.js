5d0a275ffca7c32325dca91d1f52f0338bb4d827
Gyandeep Aryan <gyandeeparyan2@gmail.com>
2023-12-22 21:38:25 +0530

    style: mobile first design && new fresh original look implemented

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import {useRouter} from 'next/navigation'
 import styles from '@/styles/home.module.css'
 import { useState } from 'react';
 import usePeer from '@/hooks/usePeer';
+import { Antenna, AudioWaveform, Plus, Video } from 'lucide-react';
 
 export default function Home() {
  const router =useRouter();
@@ -25,12 +26,27 @@ alert('enter valid room id')
 
 
   return <div className={styles.homeContainer}>
-    <h1>Samvad Setu</h1>
+   
     <div className={styles.enterRoom}>
-      <input type="text" value={roomId} onChange={(e)=>setRoomId(e?.target?.value)} />
-      <button onClick={handleJoinRoom}>Join</button>
-      <span>-------------------OR--------------------</span>
-      <button onClick={handleCreateRoom}>Create Room</button>
+      <div className='flex align-middle mt-2'>
+      <input type="text" placeholder='enter room code' value={roomId} onChange={(e)=>setRoomId(e?.target?.value)} />
+      <button onClick={handleJoinRoom} className='mx-2'><Video/></button>
+      </div>
+
+      <div className='flex text-3xl mt-60 md:mt-56'> 
+        <span className=''>
+        <Antenna className='' size={40}/>
+        </span>
+       <span className='mx-3'>samvadsetu</span>
+      
+
+      </div>
+      <span>vāgvai manasi pratiṣṭhitā</span>
+      
+    <div className='absolute bottom-8 right-6'>
+    <button onClick={handleCreateRoom}><Plus/></button>
+    </div>
+  
     </div>
   </div>;
 }
