235da8fb93119a75212286d83b31283d66a518bb
Gyandeep Aryan <gyandeeparyan.dev@gmail.com>
2024-10-26 19:44:43 +0530

    pwa data updated

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,6 @@ import { FrontFacingProvider } from '@/context/appContext'
 
 
 
-export const metadata = {
-  title: "Samvad Setu",
-  description: "Samvad Setu",
-  generator: "Next.js",
-  manifest: "/manifest.json",
-  keywords: ["nextjs", "next14", "pwa", "next-pwa"],
-};
-
 export default function App({ Component, pageProps }) {
   return (
     <FrontFacingProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <meta name='title' content='Samvad Setu' />
+        <meta name='description' content='Samvad Setu' />
+        <meta name='generator' content='Next.js' />
+        <link rel='manifest' href='/manifest.json' />
+        <meta name='keywords' content='nextjs, next14, pwa, next-pwa' />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
