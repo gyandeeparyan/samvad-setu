919e3ca976d7e4cbf62f2f8ce79770a2d8297e6a
Gyandeep Aryan <gyandeeparyan.dev@gmail.com>
2024-10-26 19:17:56 +0530

    feat: pwa configured

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
+import withPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,      // Enable React strict mode for improved error handling
+    swcMinify: true,            // Enable SWC minification for improved performance
+    compiler: {
+        removeConsole: process.env.NODE_ENV !== "development"     // Remove console.log in production
+    },
+    images: {
+      domains: ["res.cloudinary.com"],
+      remotePatterns: [{
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        
+      },],
+    },
+};
 
-module.exports = nextConfig
+export default withPWA({
+    dest: "public",         // destination directory for the PWA files
+          
+    register: true,         // register the PWA service worker
+    skipWaiting: true,      // skip waiting for service worker activation
+})(nextConfig);
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,16 @@ import '@/styles/globals.css'
 import { SocketProvider } from '@/context/socket'
 import { FrontFacingProvider } from '@/context/appContext'
 
+
+
+export const metadata = {
+  title: "Samvad Setu",
+  description: "Samvad Setu",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "next14", "pwa", "next-pwa"],
+};
+
 export default function App({ Component, pageProps }) {
   return (
     <FrontFacingProvider>
